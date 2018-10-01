16b1a355fa4c412d412e9b4a079c3a110a26e45a
gapself <gabriela.paszkiewicz@gmail.com>
2018-09-30 23:56:33 -1200

    Regression Features and Labels

--- 1_Regression_Intro.py
+++ 1_Regression_Intro.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import quandl
+import math
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -13,4 +14,16 @@ df['HL_change']=(df['Adj. Close']-df['Adj. Open']) / df['Adj. Open'] * 100.0
 
 df = df[['Adj. Close','HL_PCT','HL_change','Adj. Volume']]
 
-print(df.head())
+forecast_col = 'Adj. Close'
+#na-non a number/non available
+df.fillna(-99999, inplace=True)
+
+#math.ceil'll takie anything - rounds everything up
+#0.1 - using data that came 10 days ago to predict today
+#0.001 - predict tomorrows price
+forecast_out = int(math.ceil(0.1*len(df)))
+
+#column'll be shifted up, this way the label column for each row'll be adjusted price 10 days in the features
+df ['label'] = df[forecast_col].shift (-forecast_out)
+df.dropna(inplace=True)
+print(df.tail())
