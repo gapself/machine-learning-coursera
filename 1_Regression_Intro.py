69b9cff1f3a55e0514330ec52d2d8532f2b99d9f
gapself <gabriela.paszkiewicz@gmail.com>
2018-10-01 01:54:23 -1200

    R.Training and Testing

--- 1_Regression_Intro.py
+++ 1_Regression_Intro.py
@@ -1,8 +1,11 @@
 import pandas as pd
-import quandl
-import math
+import quandl, math
+import numpy as np
+from sklearn import preprocessing, model_selection, svm
+from sklearn.linear_model import LinearRegression
 
 df = quandl.get('WIKI/GOOGL')
+api_key='tB3urdgNfDsJMKZVPtA_'
 
 #SIMPLIFY DATA! - recreat the dataframe with meaningful features
 df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
@@ -21,9 +24,30 @@ df.fillna(-99999, inplace=True)
 #math.ceil'll takie anything - rounds everything up
 #0.1 - using data that came 10 days ago to predict today
 #0.001 - predict tomorrows price
-forecast_out = int(math.ceil(0.1*len(df)))
+forecast_out = int(math.ceil(0.01*len(df)))
+print(forecast_out)
 
 #column'll be shifted up, this way the label column for each row'll be adjusted price 10 days in the features
-df ['label'] = df[forecast_col].shift (-forecast_out)
+df['label'] = df[forecast_col].shift(-forecast_out)
 df.dropna(inplace=True)
-print(df.tail())
+
+x = np.array(df.drop(['label'],axis=1)) #our features're everything without label column
+y = np.array(df['label'])
+
+#Takes your values and attempts to normalize them to be between -1 and 1.
+# This is done for efficiency and accuracy, but is not required.﻿:
+x = preprocessing.scale(x)
+# x = x[:-forecast_out+1] #x[:29]
+#It returns a new dataframe without the column label, then it is converted into a numpy array and then it is saved in the X value
+# 1 refers to the axis,  axis=1  refers the 'column', axis=0  refers to the 'row'
+y = np.array(df['label'])
+
+x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size=0.2)
+
+# clf=LinearRegression() or the 2. algorithm:
+clf =svm.SVR(kernel='poly')
+# clf =svm.SVR()
+clf.fit(x_train,y_train)
+accuracy = clf.score(x_test,y_test)
+
+print(accuracy)
